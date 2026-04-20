d46ee83d360e6cb3fedf887f2f9a4c4da324b0d2
Rang Srivastava <rangsrivastava@gmail.com>
2026-04-20 16:21:56 +0530

    Time: 7 ms (31.91%), Space: 19.9 MB (97.72%) - LeetHub

--- 0986-interval-list-intersections/0986-interval-list-intersections.py
+++ 0986-interval-list-intersections/0986-interval-list-intersections.py
@@ -1,51 +1,34 @@
-class Solution:
-    def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
-        i=0
-        j=0
-        n=len(firstList)
-        m=len(secondList)
-        res=[]
-        while i<n and j<m:
-            start1=firstList[i][0]
-            end1=firstList[i][1]
-            start2=secondList[j][0]
-            end2=secondList[j][1]
-            if start1<start2:
-                if end1 >=start2:
-                    s=max(start1,start2)
-                    e=min(end1,end2)
-                    res.append([s,e])
-                    if end1<=end2:
-                        i+=1
-                        
-                    else: 
-                       j+=1
-                else:
+from typing import List
 
-                    i+=1
-            elif start1==start2:
-                if end1<end2:
-                    res.append([start1, end1])
-                    i+=1
-                elif end1==end2:
-                    res.append([start1, end1])
-                    i+=1
-                    
-                else:
-                    res.append([start2, end2])
-                    j+=1
+class Solution:
+    def intervalIntersection(
+        self,
+        firstList: List[List[int]],
+        secondList: List[List[int]]
+    ) -> List[List[int]]:
+
+        i = 0
+        j = 0
+        n = len(firstList)
+        m = len(secondList)
+        result = []
+
+        while i < n and j < m:
+            start1, end1 = firstList[i]
+            start2, end2 = secondList[j]
+
+            # Find overlap
+            start = max(start1, start2)
+            end = min(end1, end2)
+
+            # If valid intersection exists
+            if start <= end:
+                result.append([start, end])
+
+            # Move the pointer that ends first
+            if end1 < end2:
+                i += 1
             else:
-                    if end2 >= start1:
-                        s=max(start1,start2)
-                        e=min(end1,end2)
-                        res.append([s,e])
-                        if end1<=end2:
-                          i+=1
-                        
-                        else:
-                         j+=1
-                    else:
-                        j+=1
-        
-        return res
-        
+                j += 1
+
+        return result
